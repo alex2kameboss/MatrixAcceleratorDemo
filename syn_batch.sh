9f7c2819e696e3f588adbbfd1584332396500edc
Alexandru Puscasu <palexandru2000@gmail.com>
2025-07-04 08:18:29 +0000

    update for convolution

---
+++ syn_batch.sh
@@ -0,0 +1,14 @@
+make vivado ARGS="-prfLogP 1 -prfLogQ 1"
+make vivado ARGS="-prfLogP 1 -prfLogQ 1 -useUram"
+
+make vivado ARGS="-prfLogP 1 -prfLogQ 2"
+make vivado ARGS="-prfLogP 1 -prfLogQ 2 -useUram"
+
+make vivado ARGS="-prfLogP 2 -prfLogQ 2"
+make vivado ARGS="-prfLogP 2 -prfLogQ 2 -useUram"
+
+make vivado ARGS="-prfLogP 2 -prfLogQ 3"
+make vivado ARGS="-prfLogP 2 -prfLogQ 3 -useUram"
+
+make vivado ARGS="-prfLogP 3 -prfLogQ 3"
+make vivado ARGS="-prfLogP 3 -prfLogQ 3 -useUram"
