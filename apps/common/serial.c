7a5b7051798e8cc32fff73df87af0aa5e5313b45
Alexandru Puscasu <palexandru2000@gmail.com>
2025-08-08 15:54:52 +0300

    add uart fifo check

--- apps/common/serial.c
+++ apps/common/serial.c
@@ -2,7 +2,11 @@
 
 extern char fake_uart;
 
+#define VIVADO
+
 void _putchar(char character) {
+  char *p = (char *)0xC0000008;
+  while (p[0] & (1 << 3)) ;
   // send char to console
   fake_uart = character;
 }
