0b4aca1f86103fd73227795c2bb2b51b6cdf8dc4
Alexandru Puscasu <palexandru2000@gmail.com>
2025-10-31 13:44:27 +0200

    update freq

--- scripts/vivado/uart.tcl
+++ scripts/vivado/uart.tcl
@@ -3,5 +3,5 @@ set xilinx_uart [create_ip -name axi_uartlite -vendor xilinx.com -library ip -mo
 # User Parameters
 set_property -dict [list \
   CONFIG.C_BAUDRATE {9600} \
-  CONFIG.C_S_AXI_ACLK_FREQ_HZ_d {10} \
+  CONFIG.C_S_AXI_ACLK_FREQ_HZ_d {75} \
 ] [get_ips xilinx_uart]
