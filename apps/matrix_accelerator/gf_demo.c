e253c73de7df7c87863ca585ed9c9a4306a18c6a
sirazvan <141137940+sirazvan@users.noreply.github.com>
2026-09-18 18:59:04 +0300

    Refactor image buffer handling for stage dimensions

--- apps/matrix_accelerator/gf_demo.c
+++ apps/matrix_accelerator/gf_demo.c
@@ -2,6 +2,7 @@
 #include <stdint.h>
 #include <string.h>
 #include "guided_filter.h"
+#include "ImtMatrixAccelerator.h"
 
 
 
@@ -10,6 +11,8 @@
 
 #define TILE_SIZE 64
 #define KERNEL_SIZE 33
+#define STAGE_MAX_W (((IMG_MAX_W + TILE_SIZE - 1) / TILE_SIZE) * TILE_SIZE)
+#define STAGE_MAX_H (((IMG_MAX_H + TILE_SIZE - 1) / TILE_SIZE) * TILE_SIZE)
 
 #define MAX_BUFFER_SIZE 1024
 
@@ -66,7 +69,7 @@ uint32_t waitMessage(uint8_t* buf, uint32_t maxSize, const char* msg) {
     uint32_t size, idx;
     do {
         size = rxReadLine(buf, maxSize);
-    } while( (idx = bufStartsWith(buf, size, msg)) == -1 );
+    } while( (idx = bufStartsWith(buf, size, msg)) == UINT32_MAX );
 
     return idx;
 }
@@ -77,13 +80,14 @@ int main() {
 
     // buffers
     uint8_t buf[MAX_BUFFER_SIZE];
-    uint8_t imageInBuf[IMG_MAX_W * IMG_MAX_H * 4] __attribute__((aligned(4096)));
-    uint8_t imageOutBuf[IMG_MAX_W * IMG_MAX_H * 4] __attribute__((aligned(4096)));
+    uint8_t imageInBuf[STAGE_MAX_W * STAGE_MAX_H * 4] __attribute__((aligned(4096)));
+    uint8_t imageOutBuf[STAGE_MAX_W * STAGE_MAX_H * 4] __attribute__((aligned(4096)));
     int32_t kernel[64 * 64] __attribute__((aligned(4096)));
 
     // variables
     int imageWidth, imageHeight;
-    uint32_t numberStartIdx, imageSize;
+    uint32_t numberStartIdx;
+    int stageWidth, stageHeight, stageRowBytes;
 
     // assign buffers
     gf_rgbx_stage_input = (uint32_t*)imageInBuf;
@@ -108,17 +112,38 @@ int main() {
         //printf("height: %d\n\r", imageHeight);
         // DATA START
         //printf("Wait data start\n\r");
-        imageSize = imageWidth * imageHeight * 4;
+        stageWidth = ((imageWidth + TILE_SIZE - 1) / TILE_SIZE) * TILE_SIZE;
+        stageHeight = ((imageHeight + TILE_SIZE - 1) / TILE_SIZE) * TILE_SIZE;
+        stageRowBytes = stageWidth * 4;
         waitMessage(buf, MAX_BUFFER_SIZE, "$DATA_START");
         // DATA INPUT
         //printf("Data\n\r");
-        for ( int i = 0; i < imageSize; ++i ) {
-            imageInBuf[i] = rxReadByte();
+        for (int y = 0; y < imageHeight; y++) {
+            int rowBase = y * stageRowBytes;
+            for (int x = 0; x < imageWidth * 4; x++) {
+                imageInBuf[rowBase + x] = rxReadByte();
+            }
+            for (int x = imageWidth; x < stageWidth; x++) {
+                for (int channel = 0; channel < 4; channel++) {
+                    imageInBuf[rowBase + x * 4 + channel] =
+                        imageInBuf[rowBase + (imageWidth - 1) * 4 + channel];
+                }
+            }
+        }
+        for (int y = imageHeight; y < stageHeight; y++) {
+            int rowBase = y * stageRowBytes;
+            for (int x = 0; x < stageRowBytes; x++) {
+                imageInBuf[rowBase + x] = 0;
+            }
+        }
+        for (int i = 0; i < stageHeight * stageRowBytes; i++) {
+            imageOutBuf[i] = imageInBuf[i];
         }
         // DATA END
         //printf("\n\rWait data end\n\r");
         waitMessage(buf, MAX_BUFFER_SIZE, "$DATA_END");
         // EXECUTE
+        FLUSH_D_CACHE();
         guided_filter_acc (
             imageWidth,
             imageHeight,
@@ -133,8 +158,11 @@ int main() {
         //    imageOutBuf[i] = imageInBuf[i];
         //}
         printf("$RESULT_IMAGE\r\n");
-        for ( int i = 0; i < imageSize; ++i ) {
-            txSendByte(imageOutBuf[i]);
+        for (int y = 0; y < imageHeight; y++) {
+            int rowBase = y * stageRowBytes;
+            for (int x = 0; x < imageWidth * 4; x++) {
+                txSendByte(imageOutBuf[rowBase + x]);
+            }
         }
         printf("$RESULT_METRICS\r\n");
         printf("hw,lanes,img_w,img_h,tile_w,tile_h,box_w,box_h,#tiles,tile_load,tile_compute,tile_store,total,MA_VS_ADD,MA_VS_MULT,MA_VS_SRA,MA_VS_SRL,MA_VV_ADD,MA_VV_CNV,MA_VV_NW,MA_VV_SMULT,MA_VV_SUB,MA_DEFINE_int32_t,MA_LOC_RECT\r\n");
